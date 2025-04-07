65c0d5d6510a2fe38f33ec595a380098065085c5
dlwldn34 <dlwldn34@gmail.com>
2025-04-07 15:29:45 +0900

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+class Student {
+  String name;
+  int score;
+
+  Student(this.name, this.score);
+}
+
+class StudentManagement {
+  List<Student> students = [];
+
+  void addStudent(String name, int score) {
+    students.add(Student(name, score));
+  }
+
+  void studentsList() {
+    for (var student in students) {
+      print(
+        '이름: ${student.name}, 점수: ${student.score}',
+      );
+    }
+  }
+
+  double average() {
+    int sum = 0;
+    for (var student in students) {
+      sum += student.score;
+    }
+    return sum / students.length;
+  }
+}
+
+void main() {
+  print('학생 성적 관리 프로그램');
+  print('1. 학생 추가');
+  print('2. 학생 목록 출력');
+  print('3. 평균 점수계산');
+  print('4. 종료');
+  print('메뉴를 선택하세요:');
+
+  int choice = int.parse(stdin.readLineSync()!);
+
+  StudentManagement studentManagement = StudentManagement();
+
+  for (;;) {
+    if (choice == 1) {
+      stdout.write('학생 이름을 입력하세요:');
+      String name = stdin.readLineSync()!;
+      stdout.write('학생 점수를 입력하세요:');
+      int score = int.parse(stdin.readLineSync()!,);
+      print('$name학생이 추가되었습니다.');
+    } else if (choice == 2) {
+      print('학생 목록: ');
+      studentManagement.studentsList();
+    } else if (choice == 3) {
+      print('평균 점수: ');
+      studentManagement.average();
+    } else if (choice == 4) {
+      print('프로그램을 종료합니다.');
+      break;
+    }
+  }
+}
